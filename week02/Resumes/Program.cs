c18b036e92e18750915dc35f8178613121571a32
sherrypeach <casperche27gmail.com>
2025-03-13 12:23:23 -0300

    week 02

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,64 @@
 using System;
+using System.Collections.Generic;
 
+// Job class stores details of a job
+class Job
+{
+    public string Company { get; set; }
+    public string JobTitle { get; set; }
+    public int StartYear { get; set; }
+    public int EndYear { get; set; }
+
+    public Job(string company, string jobTitle, int startYear, int endYear)
+    {
+        Company = company;
+        JobTitle = jobTitle;
+        StartYear = startYear;
+        EndYear = endYear;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"{JobTitle} ({Company}) {StartYear}-{EndYear}");
+    }
+}
+
+// Resume class stores a person's name and a list of jobs
+class Resume
+{
+    public string Name { get; set; }
+    private List<Job> Jobs { get; set; }
+
+    public Resume(string name)
+    {
+        Name = name;
+        Jobs = new List<Job>();
+    }
+
+    public void AddJob(Job job)
+    {
+        Jobs.Add(job);
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"Resume for: {Name}\n");
+        foreach (var job in Jobs)
+        {
+            job.Display();
+        }
+    }
+}
+
+// Main program to test the classes
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Resume myResume = new Resume("John Doe");
+        myResume.AddJob(new Job("Microsoft", "Software Engineer", 2019, 2022));
+        myResume.AddJob(new Job("Apple", "Product Designer", 2022, 2024));
+        
+        myResume.Display();
     }
 }
